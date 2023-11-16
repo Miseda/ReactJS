b0dd2d3826bbbdc2b0526e8f0576bce87e2a657b
Miseda <misedaalex@gmail.com>
2023-11-17 00:15:12 +0300

    Added eventhandlers to the buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+const clickHandler = (e) => {
+
+        console.log("Hey Miseda", e.target);
+}
+
+const clickHandlerAgain = (name, e) => {
+        console.log("Hey " + name, e.target);
+}
+
 const Home = () => {
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={clickHandler} > Click me</button>
+            <button onClick = { (e) => clickHandlerAgain("Alex", e)} >Click me again</button>
         </div>
      );
 }
