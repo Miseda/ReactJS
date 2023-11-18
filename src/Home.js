55bfe8af041be36174f270e5152957d1ae026a45
Miseda <misedaalex@gmail.com>
2023-11-19 00:56:50 +0300

    Used a useState hook on the react app

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,17 @@ import { useState } from "react";
 const Home = () => {
 
     const [name, setName] = useState("Alex")
+    const [age, setAge] = useState(0)
 
     const clickHandler = () => {
         setName("Job");
+        setAge(70);
 }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is  {age} </p>
             <button onClick={clickHandler} > Click me</button>
         </div>
      );
